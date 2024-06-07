5d2490fe3d333c2d96f2e264f0f6995b9abf1930
francoasevey <francoasevey@gmail.com>
2024-06-06 23:59:42 -0300

    functions

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+// functions.php
+
+function validate_login($username, $password) {
+    // Lógica para validar el inicio de sesión
+}
+
+function create_exam($data) {
+    // Lógica para crear una mesa de examen
+}
+
+// Otras funciones útiles...
+?>
