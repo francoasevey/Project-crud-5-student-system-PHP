c3bd83d61b09325278c71a2e7ec046f1dd22874b
francoasevey <francoasevey@gmail.com>
2024-06-07 00:03:38 -0300

    solicitudes.php

--- db-connection.php
+++ db-connection.php
@@ -4,9 +4,14 @@ $db_user = "root";
 $db_pass = "";
 $db_name = "systemalumnos";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+        if (!$link) {
+            echo "Error: no se puede conectar a MYSQL." . PHP_EOL;
+            echo "<br>";
+            echo "error de depuración: " . mysqli_connect_errno() . PHP_EOL;
+            echo "<br>";
+            echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+            exit();
+        }
 ?>
---
+++ solicitudes.php
@@ -0,0 +1,6 @@
+<?php
+// index.php (y otras páginas similares)
+include_once 'includes/db_connection.php';
+include_once 'includes/functions.php';
+// Aquí va el código HTML y PHP para la página
+?>
